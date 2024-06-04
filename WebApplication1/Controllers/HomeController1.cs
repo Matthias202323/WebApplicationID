b2a4cccbbba06f222d9c03e57565758c2a19a391
MatthiasDesmoulins <Matthias-Externe.Desmoulins@asmeg.org>
2024-06-04 20:21:56 +0200

    Ajoutez des fichiers projet.

---
+++ WebApplication1/Controllers/HomeController1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class SalaryController
+    {
+        private ISalaryCalculator _calculator;
+
+        public SalaryController(ISalaryCalculator injectedCalculator)
+        {
+            _calculator = injectedCalculator;
+        }
+
+        [HttpGet("{yearlyAmount}")]
+        public Double Get(Double yearlyAmount)
+        {
+            Double calculatedSalary = _calculator.CalculateSalary(yearlyAmount);
+            return calculatedSalary;
+        }
+    }
+}
---
+++ WebApplication1/Services/QuartelyrSalarlyCalcolator.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Services
+{
+    public class QuartelyrSalarlyCalcolator: ISalaryCalculator
+    {
+        public Double CalculateSalary(Double yearlyAmount)
+        {
+            return yearlyAmount / 4;
+        }
+    }
+}
